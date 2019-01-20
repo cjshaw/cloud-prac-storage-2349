fe8e984104adfa69c48a860df3561d871d4506b9
Clint Shaw <shaw.clint@gmail.com>
2019-01-19 20:07:07 -0800

    upload time!

--- script.py
+++ script.py
@@ -53,16 +53,10 @@ def run_sample():
         block_blob_service.set_container_acl(container_name, public_access=PublicAccess.Container)
 
         # Create a file in Documents to test the upload and download.
-        local_path=os.path.expanduser("~/Documents")
-        local_file_name ="QuickStart_" + str(uuid.uuid4()) + ".txt"
+        local_path=os.path.expanduser("~/images")
+        local_file_name ="testimage.jpg"
         full_path_to_file =os.path.join(local_path, local_file_name)
 
-        # Write text to the file.
-        file = open(full_path_to_file,  'w')
-        file.write("Hello, World!")
-        file.close()
-
-        print("Temp file = " + full_path_to_file)
         print("\nUploading to Blob storage as blob" + local_file_name)
 
         # Upload the created file, use local_file_name for the blob name
@@ -74,24 +68,8 @@ def run_sample():
         for blob in generator:
             print("\t Blob name: " + blob.name)
 
-        # Download the blob(s).
-        # Add '_DOWNLOADED' as prefix to '.txt' so you can see both files in Documents.
-        full_path_to_file2 = os.path.join(local_path, str.replace(local_file_name ,'.txt', '_DOWNLOADED.txt'))
-        print("\nDownloading blob to " + full_path_to_file2)
-        block_blob_service.get_blob_to_path(container_name, local_file_name, full_path_to_file2)
-
-        sys.stdout.write("Sample finished running. When you hit <any key>, the sample will be deleted and the sample "
-                         "application will exit.")
-        sys.stdout.flush()
-        input()
-
-        # Clean up resources. This includes the container and the temp files
-        block_blob_service.delete_container(container_name)
-        os.remove(full_path_to_file)
-        os.remove(full_path_to_file2)
     except Exception as e:
         print(e)
-
 
 # Main method.
 if __name__ == '__main__':
